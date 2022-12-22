2ad31454578561a439d197b8efff3e6b8f7fb99a
KacperWojdak <kacper.wojdak@interia.pl>
2022-12-22 21:27:45 +0100

    .

--- CardGame/Menu.php
+++ CardGame/Menu.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <head>
-    <link rel="stylesheet" href="CSS/menu.css" type="text/css">
+    <link rel="stylesheet" href="menu.css" type="text/css">
     <title>Menu</title>
 </head>
 
 
 <body>
-    <div class="Menu">
+    <div class="menu">
         <h1>MENU</h1>
         <h1>Play as :</h1>
     </div>
 
-    <div class="Buttons">
-        <button class="button1"><a href="Menu2.php">Waterbender</a></button><br>
-        <button class="button2"><a href="Menu2.php">Airbender</a></button><br>
-        <button class="button3"><a href="Menu2.php">Earthbender</a></button><br>
-        <button class="button4"><a href="Menu2.php">Firebender</a></button><br>
+    <div class="buttons">
+        <button class="button water"><a href="menu2.php"><span>Waterbender</span></a></button><br>
+        <button class="button air"><a href="menu2.php"><span>Airbender</span></a></button><br>
+        <button class="button earth"><a href="menu2.php"><span>Earthbender</span></a></button><br>
+        <button class="button fire"><a href="menu2.php"><span>Firebender</span></a></button><br>
     </div>
     
 </body>
