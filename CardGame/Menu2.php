7ef41e344556fd1feb5fee7d63a8d7a79b9e6f8c
KacperWojdak <kacper.wojdak@interia.pl>
2022-12-22 21:32:26 +0100

    Update Menu2.php

--- CardGame/Menu2.php
+++ CardGame/Menu2.php
@@ -12,7 +12,7 @@
     </div>
 
     <div class="buttons">
-    <button class="button water"><a href="menu2.php"><span>Waterbender</span></a></button><br>
+        <button class="button water"><a href="menu2.php"><span>Waterbender</span></a></button><br>
         <button class="button air"><a href="menu2.php"><span>Airbender</span></a></button><br>
         <button class="button earth"><a href="menu2.php"><span>Earthbender</span></a></button><br>
         <button class="button fire"><a href="menu2.php"><span>Firebender</span></a></button><br>
