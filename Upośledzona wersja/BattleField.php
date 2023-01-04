5942683feeac51cebcf3b9b2051c373e78e05ce3
AndrzejBudzyn <andrzej.budzyn@studenci.collegiumwitelona.pl>
2023-01-04 18:41:58 +0100

    Już jest podstawa jakaś

--- Upośledzona wersja/BattleField.php
+++ Upośledzona wersja/BattleField.php
@@ -1,20 +1,32 @@
 <?php
+
 namespace Battle;
+
+use Exception;
+
+/////////////////////////////////////////       INNE                  ////////////////////////////////////////////////////////////
 final class Log {
 
     public static function info(string $message = ""): void {
         echo $message .  PHP_EOL;
     }
 }
-class MainCard{ 
-    private $HP=40;
-    private $deck;
 
+
+class WinnerWasCalled extends Exception{}
+/////////////////////////////////////////////////         MAIN CARDS             //////////////////////////////////////////////////////////////
+class MainCard{ 
+    private int $HP=40;
+    private $deck;
+    private  $name;
     public function CardGame(MainCard $deck){
         $this->deck=$deck;
     }
     public function GetHp(){
         return $this->HP;
+    }
+    public function GetName(){
+        return $this->name;
     }
     public function  ChangeHP(int $mod){
          $this->HP=$this->HP-$mod;
@@ -31,7 +43,7 @@ interface PlayerInterface {
 
 }
 class PlayerCardFire extends MainCard implements PlayerInterface{
-
+    private $name="Fire";
     public function GetPassive(): int{
     
         $random=random_int(1,10);
@@ -50,7 +62,7 @@ class PlayerCardFire extends MainCard implements PlayerInterface{
 }
 
 class PlayerCardWater extends MainCard implements PlayerInterface {
-
+    private $name="Water";
     public function GetPassive(): int{
         
         $random=random_int(1,10);
@@ -67,12 +79,57 @@ class PlayerCardWater extends MainCard implements PlayerInterface {
 
     }
     }
+/////////////////////////////////////////////////         CLASSES        /////////////////////////////////////////////////
+    abstract class Card {
+        private $NameOfCard;
+        private $EnergyCost=0;
+        private $type="";
+        private $Decription="";
+       
+        public function getCardName() {
+            return $this->NameOfCard;
+        }
+        public function WhatTypeIsIT() {
+            echo $this->type;
+        }
+
+        public function __construct(int $energycost, string $nameofcard, string $decription)
+    {
+        $this->EnergyCost=$energycost;
+        $this->NameOfCard=$nameofcard;
+        $this->Decription=$decription;
+    }
+
+    
+
+    }
+
+    class Deck {
+        protected $ListOfCard;
+        protected $Card;
+
+        public function __construct($Card) {
+            $this->Card = $Card;
+            shuffle($this->Card);
+        }
+
+
+        public function ReadylistCards() {
+            foreach($this->Card as $Card) {
+                echo $Card . '<br />';
+            }  
+        }
+    }
+
+    
+    
 
     class Board {
-        protected $Player;
-        protected $Enemy;
+        protected MainCard $Player;
+        protected MainCard $Enemy;
         protected $Mana=0;
         protected $TurnCounter=0;
+        protected $winner;
 
 
         public function __construct(MainCard $player,MainCard $enemy,) {
@@ -80,11 +137,15 @@ class PlayerCardWater extends MainCard implements PlayerInterface {
             $this->Enemy = $enemy;
         }
 
-
+        public function GetWinner(){
+            return $this->winner;
+        }
 
         public function PlayTurn(){
+            Log::info();
+            Log::info();
             $this->TurnCounter++;
-            echo $this->TurnCounter;
+            Log::info("Trwa Tura:  $this->TurnCounter") ;
             $i=0;
             if($this->Mana <=10){
                 $this->Mana++;
@@ -96,15 +157,48 @@ class PlayerCardWater extends MainCard implements PlayerInterface {
             }while($i!=1);
             $eHP=$this->Enemy->GetHp();
             $pHP=$this->Player->GetHp();
-            Log::info("Hp twoje $pHP, Hp Enemy $eHP") ;
+            Log::info("Twoje życie wynosi:  $pHP") ;
+            Log::info("Życie twojego przeciwnika wynosi:  $eHP") ;
             if($eHP<=0 || $pHP<=0){
-                return False;
+                if($eHP>$pHP){
+                $this->winner = "Przeciwnik";
+				throw new WinnerWasCalled();
+                }
+                if($pHP>$eHP){
+                    $this->winner = "Gracz";
+                    throw new WinnerWasCalled();
+                    }
             };
         }
 
 
     }
 
+//////////////////////////////////////////////////                ActionCARDS           ///////////////////////////////////////////////////
+    class DefensiveCards extends Card  {
+        private $type="Defensive";
+    
+    
+       
+       
+    
+    }
+    class OffensiveCards extends Card {
+        private $type="Offensive";
+        
+     
+        
+    }
+
+
+    class SpecialCards extends Card {
+        private $type="Special";
+        
+       
+    
+        
+    
+    }
 
 ?>
 <!DOCTYPE html>
@@ -122,9 +216,14 @@ class PlayerCardWater extends MainCard implements PlayerInterface {
         $you=new PlayerCardWater();
         $notyou= new PlayerCardFire();
         $board= new Board($you,$notyou);
-        do{
-           $koniec=$board->PlayTurn();
-        }while($koniec!=false)
+        try {
+            while(true) {
+                $board->PlayTurn();
+            }
+        }catch(WinnerWasCalled $exception) {
+            Log::info();
+            Log::info($board->GetWinner() . " wygrał.");
+        }
       
 
 
