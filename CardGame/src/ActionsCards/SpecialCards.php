2c6e188d713322ef909ef55d4bb12c3c0971adb6
AndrzejBudzyn <andrzej.budzyn@studenci.collegiumwitelona.pl>
2023-01-15 21:06:27 +0100

    Update SpecialCards.php

--- CardGame/src/ActionsCards/SpecialCards.php
+++ CardGame/src/ActionsCards/SpecialCards.php
@@ -7,10 +7,7 @@ use Game\Board\Card;
 class SpecialCards extends Card {
     private $type = "Special";
         
-    public function DrawCard() {}
-    public function Nothing() {}
-    public function RegenMana() {}
-    public function Pass() {}
+
     
 }
 
